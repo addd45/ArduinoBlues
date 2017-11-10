e842e640fea79a003cfcf8cf2dc31da1daa5c4a3
Huss <addd45@gmail.com>
2017-11-10 15:38:36 -0600

    2nd
    still WIP

--- BluesBoard/BluesBoard/BluesBoard.ino
+++ BluesBoard/BluesBoard/BluesBoard.ino
@@ -6,21 +6,25 @@
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
 #include <ArduinoJson.h>
+#include <LiquidCrystal_I2C.h>
 
 const char* ssid = "WifiMcWifiFace";
 const char* password = "weakpassword";
 const char* mqtt_server = "hussbus.duckdns.org";
+const char* mqtt_out_topic = "outTopic";
+const char* mqtt_in_topic = "HA/Other/BluesScore";
 
 WiFiClient espClient;
 PubSubClient client(espClient);
 StaticJsonBuffer<200> jsonBuffer;
+long lastReconnectAttempt = 0;
 
 void setup() {
   Serial.begin(115200);
   setup_wifi();
   client.setServer(mqtt_server, 1883);
   client.setCallback(mqtt_callback);
-
+  lastReconnectAttempt = 5000;
 }
 void setup_wifi() {
   delay(10);
@@ -42,39 +46,60 @@ void setup_wifi() {
   Serial.println(WiFi.localIP());
 }
 
-void mqtt_callback(char* topic, byte* payload, unsigned int length) {
-  JsonObject& rootJson = jsonBuffer.parseObject(payload);
-  Serial.print("Message arrived [");
-  Serial.print(topic);
-  Serial.print("] ");
-  for (int i = 0; i < length; i++) {
-    Serial.print((char)payload[i]);
-  }
-  Serial.println();
+bool reconnect(){
+  Serial.println("Connecting MQTT...");
+  return client.connect("ESP8266Client", "homeassistant", "sb4517");
 }
 
-void reconnect() {
-  // Loop until we're reconnected
-  while (!client.connected()) {
-    Serial.print("Attempting MQTT connection...");
-    // Attempt to connect
-    if (client.connect("ESP8266Client", "homeassistant", "sb4517")) {
-      Serial.println("connected");
-      // Once connected, publish an announcement...
-      client.publish("outTopic", "hello world");
-      // ... and resubscribe
-      client.subscribe("hass/other/BluesScoreUpdate");
-    } else {
-      Serial.print("failed, rc=");
-      Serial.print(client.state());
-      Serial.println(" try again in 5 seconds");
-      // Wait 5 seconds before retrying
-      delay(5000);
+void mqtt_callback(char* topic, byte* payload, unsigned int length) {
+  char message[length];
+  
+  Serial.println("Message arrived!");
+
+  for (int i = 0; i < length; i++) {
+    message[i] = (char)payload[i];
+  }
+  Serial.print("payload: ");
+  Serial.println(message);
+
+  JsonObject& rootJson = jsonBuffer.parseObject(message);
+  parseJson(rootJson);
+}
+
+void mqtt_SendTopic(char* payload){
+  // publish an announcement...
+  client.publish(mqtt_out_topic, payload);
+  // ... and resubscribe
+} 
+
+void parseJson(JsonObject& rootJson){
+  //Json parse failed?
+  if (!rootJson.success()) {
+    Serial.println("parseObject() failed");
+    return;
+  }
+  else{
+    int blues = rootJson["blues"];
+    Serial.println("Parsed Object just fine.");
+    Serial.print("blues value: ");
+    Serial.print(blues);
+  }  
+}
+
+void loop()
+{
+  if (!client.loop()) {
+    long now = millis();
+    if (now - lastReconnectAttempt > 5000) {
+      lastReconnectAttempt = now;
+      // Attempt to reconnect
+      if (reconnect()) {
+        lastReconnectAttempt = 0;
+    client.subscribe(mqtt_in_topic);  
+    Serial.println("Reconnected to mqtt");
+      }
     }
   }
-}
-
-void loop() {
-  // put your main code here, to run repeatedly:
 
 }
+
